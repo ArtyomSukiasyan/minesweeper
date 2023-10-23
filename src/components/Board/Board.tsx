cf142a653be3552e3e97440de22bbf4b1bc2d15d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-10-23 10:25:44 +0400

    fix: fix mobile ui

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -4,26 +4,29 @@ import styles from "./Board.module.css";
 
 export default function Board({ bombs, visited, visitCell }: IBoard) {
   return (
-    <>
+    <div className={styles.board}>
       {bombs.map((arr: any[], index: number) => (
         <div key={index}>
           {arr.map((elem: any[], i: number) => (
-            <div
+            <span
               key={i}
               onClick={() => visitCell(index, i)}
-              className={
-                visited[index][i] === 0 ? styles.buttonStyle : styles.visitStyle
-              }
+              className={`${styles.square}
+                ${
+                  visited[index][i] === 0
+                    ? styles.buttonStyle
+                    : styles.visitStyle
+                }`}
             >
               {visited[index][i] === 0
                 ? null
                 : bombs[index][i] === 0
                 ? emptyString
                 : bombs[index][i]}
-            </div>
+            </span>
           ))}
         </div>
       ))}
-    </>
+    </div>
   );
 }
